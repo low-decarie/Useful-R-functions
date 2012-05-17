9200b1c6d34744a78ede1209c5ad6325eade13db
Etienne <etienne.low-decarie@mail.mcgill.ca>
2012-05-17 13:33:22 -0400

    correcter file name

--- Data import/Microplates/read microplate list.R
+++ Data import/Microplates/read microplate list.R
@@ -2,14 +2,14 @@ read.microplate.list<-function(file.list="", directory=".", ext="TXT",...){
 
   
   if(file.list==""){
-  file.list<-paste(directory,"/", list.files(path=directory, pattern=ext), sep="")
+  file.list<-list.files(path=directory, pattern=ext)
   }
   
   readings<-NULL
   for(file in file.list){
     print(file)
     try({
-        readings.temp<-read.microplate(file,...)
+        readings.temp<-read.microplate(paste(directory,"/",file, sep=""),...)
         readings.temp$file<-file
         readings<-rbind(readings, readings.temp)
         })
