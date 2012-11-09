94367811c9f7efbb20559f1cce98a3d85900e38b
Etienne Low-Decarie <etienne.low-decarie@mail.mcgill.ca>
2012-11-09 08:58:42 -0500

    up

--- R packages/install package list.R
+++ R packages/install package list.R
@@ -1,5 +1,5 @@
 if(!require(plyr))install.packages("plyr")
 
-load("~/Documents/Useful-R-functions/R packages/package_list.RData")
+load("./R/package_list.RData")
 
 l_ply(package.list, install.packages)
